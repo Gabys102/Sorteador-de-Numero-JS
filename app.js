2a68b1dab7c1e6fd72296abdfceb7168a6d07b7e
Gabrielle Costa <costa.bry102@gmail.com>
2026-04-23 13:10:01 -0300

    corrige lógica do sorteador

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ function sortear() {
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
+    if (de >= ate) {
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        return;
+    }
+
     if (quantidade > (ate - de + 1)) {
         alert('A quantidade de números sorteados não pode ser maior que o intervalo.');
         return;
@@ -22,14 +27,9 @@ function sortear() {
     }
 
     let resultado = document.getElementById('resultado');
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
-
+    resultado.innerHTML = `Números sorteados: ${sorteados}`;
     alterarStatusBotao();
 }
-  if (de >= ate) {
-    alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
-    return;
-  }
 
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -51,7 +51,6 @@ function reiniciar() {
     document.getElementById('quantidade').value = '';
     document.getElementById('de').value = '';
     document.getElementById('ate').value = '';
-    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
-
+    document.getElementById('resultado').innerHTML = 'Números sorteados: nenhum até agora';
     alterarStatusBotao();
 }
